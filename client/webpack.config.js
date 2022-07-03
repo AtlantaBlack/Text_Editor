498a8fdfe5608dd1dd271aaa6ecf76322536190a
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-03 17:26:16 +1000

    filled out client webpack.config with output, plugins, rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,79 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: "development",
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js"
+		},
+		output: {
+			// bundle output
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist")
+		},
+		plugins: [
+			// webpack plugin for generating the index.html file and injecting bundles
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "JATE"
+			}),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+			// plugin to minify css
+			new MiniCssExtractPlugin(),
+
+			// plugin to inject custom service worker
+			new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js"
+			}),
+
+			// plugin to create manifest.json file
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: "Just Another Text Editor",
+				short_name: "J.A.T.E",
+				description: "A browser-based text editor",
+				background_color: "#FFFFFF",
+				theme_color: "#463F57", // dark purple colour
+				start_url: "/",
+				publicPath: "/",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons")
+					}
+				]
+			})
+		],
+		module: {
+			rules: [
+				{
+					// test for .css files and use mini-css plugin on them
+					test: /\.css$/i,
+					use: [MiniCssExtractPlugin.loader, "css-loader"]
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					// use babel-loader for backwards compatibility (convert es6 to es5 if needed)
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"] // '@babel/preset-env' should include plugins '@babel/plugin-proposal-object-rest-spread' & '@babel/transform-runtime'
+						}
+					}
+				}
+			]
+		}
+	};
 };
