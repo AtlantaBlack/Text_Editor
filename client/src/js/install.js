11a6f056cfb6b42c09bac2fe60b31c8d84c37faf
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-03 17:41:18 +1000

    added install code for app installation

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,35 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// reference: https://web.dev/codelab-make-installable/
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// `beforeinstallprompt` event handler
+window.addEventListener("beforeinstallprompt", (event) => {
+	// stash the event so it can be triggered later
+	window.deferredPrompt = event;
+	// remove hidden class from install button
+	butInstall.classList.toggle("hidden", false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// event handler for clicking on `butInstall` element
+butInstall.addEventListener("click", async () => {
+	// this is coming from the 'before install prompt' function
+	const promptEvent = window.deferredPrompt;
+
+	// if there is no deferred prompt, exit out/do nothing
+	if (!promptEvent) {
+		return;
+	}
+	// otherwise, show install prompt
+	promptEvent.prompt();
+	// reset the deferred prompt value as prompt() can only be called once
+	window.deferredPrompt = null;
+	// add hidden class to install button
+	butInstall.classList.toggle("hidden", true);
+});
+
+// `appinstalled` event handler
+window.addEventListener("appinstalled", (event) => {
+	// clear the deferred prompt value
+	window.deferredPrompt = null;
+});
